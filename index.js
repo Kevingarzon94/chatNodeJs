8d5277467d62bb520ee7325f4be76e4bbec58a0e
kevin-garzon@accionplus.com <kevin-garzon@accionplus.com>
2019-01-21 16:59:02 -0500

    puerto para desplegar en heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname+'/view/index.html');
@@ -17,6 +18,6 @@ io.on('connection', socket=>{
 
 io.emit('some event', {for: 'everyone'});
 
-http.listen(3000, ()=>{
+http.listen(port, ()=>{
     console.log('Listening on *:3000');
 });
